855158c8d669923c21cfe196dfd26a486c562a59
yjglab <yjgdesign@gmail.com>
2020-11-02 23:49:01 +0900

    completion. first.

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-canvas.width = 700;
-canvas.height = 700;
+const cursorImg = [
+    "cursor-pencil-01.cur",
+    "cursor-bucket-01.cur",
+  ];
 
-ctx.strokeStyle = "#2c2c2c";
-ctx.lineWidth = 2.5;
+const INITIAL_COLOR = "#2c2c2c"
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 5.0;
+
 
 let painting = false;
+let filling = false;
+
 
 function stopPainting() {
     painting = false;
@@ -25,23 +44,79 @@ function onMouseMove(event){
 
     } else {
         ctx.lineTo(x, y);
-        ctx.stroke() // 마우스 움직이는 내내 발생.
+        ctx.stroke() 
     }
 }
 
-function onMousedown(event){
-    painting = true;
+
+
+
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
 
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+        document.body.style.cursor = 'url("' + cursorImg[0] + '"), default';
+    } else {
+        filling = true;
+        mode.innerText = "paint";
+        document.body.style.cursor = 'url("' + cursorImg[1] + '"), default';
+        
+    }
+}
 
+function handleCanvasClick(){
+    if(filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+function handleCM(event){ 
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a"); 
+    link.href = image;
+    link.download = "SUPER_PAINTER-artwork";
+    link.click(); 
+}
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", startPainting); // 누를 때
-    canvas.addEventListener("mouseup", stopPainting); // 놓을 때
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting); 
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM)
+}
+if(colors) {
+    Array.from(colors).forEach(c => 
+        c.addEventListener("click", handleColorClick)
+    );
+}
+        
+if(range) {
+    range.addEventListener("input", handleRangeChange)
 }
 
-// clientX,Y : 윈도우 전체의 범위 내에서 마우스 위치값.
-// offsetX,Y : 설정한 canvas 범위 내에서 마우스 위치값.
+if(mode) {
+    mode.addEventListener("click", handleModeClick)
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick)
+}
+
+
